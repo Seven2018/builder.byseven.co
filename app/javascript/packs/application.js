5a03088cb80a93e76d20ea1acf8fe82a92a17c9d
Brice Chapuis <bricechapuis>
2021-03-17 17:48:18 +0100

    js working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,5 @@
 import "bootstrap";
 require("../plugins/flatpickr")
-// import {initSortable} from "../plugins/init_sortable.js";
-import 'easy-autocomplete/dist/jquery.easy-autocomplete';
-import 'stylesheets/application';
-// initSortable();
-// global.initSortable = initSortable;
-
 
 try {
   require("trix")
