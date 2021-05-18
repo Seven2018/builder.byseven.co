5501041f41e4a3f209fad2a12d3f323234e6435f
Brice Chapuis <bricechapuis>
2021-05-18 12:11:30 +0200

    Calendar fix

--- app/controllers/session_trainers_controller.rb
+++ app/controllers/session_trainers_controller.rb
@@ -3,7 +3,11 @@ class SessionTrainersController < ApplicationController
     skip_authorization
     client = Signet::OAuth2::Client.new(client_options)
     # Allows to pass informations through the Google Auth as a complex string
-    client.update!(state: Base64.encode64(params[:list]+','+params[:session_id]+','+params[:to_delete]))
+    if params[:user_ids].present?
+      client.update!(state: Base64.encode64(params[:list]+','+params[:session_id]+','+params[:training_id]+','+params[:user_ids]))
+    else
+      client.update!(state: Base64.encode64(params[:list]+','+params[:session_id]+','+params[:training_id]))
+    end
     redirect_to client.authorization_uri.to_s
   end
 
@@ -17,12 +21,115 @@ class SessionTrainersController < ApplicationController
 
   def calendars
     skip_authorization
-    UpdateCalendarJob.perform_async(params[:code], params[:state], params[:scope], client_options)
-    session_ids = Base64.decode64(params[:state]).split('|')[1].split(',').map{|x| x.to_i}
-    command = Base64.decode64(params[:state]).split('|').first.split(',').first
-    training_id = Base64.decode64(params[:state]).split('|').first&.split(',')&.last
+    training_id = Base64.decode64(params[:state]).split('/')[1]
     training = Training.find(training_id)
-    training = Session.find(session_ids[0]).training unless training.present?
+    # UpdateCalendarJob.perform_async(params[:code], params[:state], params[:scope], client_options)
+    # Gets clearance from OAuth
+    client = Signet::OAuth2::Client.new(client_options)
+    client.code = params[:code]
+    client.update!(client.fetch_access_token!)
+    # Initiliaze GoogleCalendar
+    service = Google::Apis::CalendarV3::CalendarService.new
+    service.authorization = client
+    # Get the targeted session
+    session_ids = Base64.decode64(params[:state]).split('|')[1].split(',').map{|x| x.to_i}
+    command = Base64.decode64(params[:state]).split('|').first
+    # Calendars ids
+    calendars_ids = {'other' => 'vum1670hi88jgei65u5uedb988@group.calendar.google.com'}
+    User.where(access_level: ['super admin','admin']).each{|x| calendars_ids[x.id] = x.email}
+    # Lists the users and the ids of the events to be deleted
+    if command[0...-1] == 'purge_session'
+      SessionTrainer.where(session_id: session_ids).each do |session_trainer|
+        begin
+          delete_calendar_id(session_trainer, service)
+        rescue
+        end
+      end
+      Session.find(session_ids.join).destroy
+      redirect_to training_path(training)
+      return
+    elsif command[0...-1] == 'remove_trainers' || command[0...-1] == 'remove_trainers_global'
+      trainers_to_delete = Base64.decode64(params[:state]).split('#')[1].split(',')
+      SessionTrainer.where(id: trainers_to_delete).each do |session_trainer|
+        begin
+          delete_calendar_id(session_trainer, service)
+        rescue
+        end
+        session_trainer.destroy
+      end
+      redirect_to training_path(training)
+      return
+    else
+      SessionTrainer.where(session_id: session_ids).each do |session_trainer|
+        begin
+          delete_calendar_id(session_trainer, service)
+          session_trainer.update(calendar_uuid: nil)
+        rescue
+        end
+      end
+      # Lists the users for whom an event will be created
+      list = command.split(',')
+      # Creates the event in all the targeted calendars
+      list.each do |ind|
+        Session.where(id: session_ids).each do |session|
+          date = session&.date
+          day, month, year = date.day, date.month, date.year
+          start_time = session.start_time.change(day: day, month: month, year: year)
+          end_time = session.end_time.change(day: day, month: month, year: year)
+          events = []
+          break_position = session.workshops.find_by(title: 'Pause Déjeuner')&.position
+          if break_position.nil?
+            events << Google::Apis::CalendarV3::Event.new({
+              start: {
+                date_time: start_time.rfc3339.slice(0...-1),
+                time_zone: 'Europe/Paris',
+              },
+              end: {
+                date_time: end_time.rfc3339.slice(0...-1),
+                time_zone: 'Europe/Paris',
+              },
+              summary: session.training.client_company.name + " - " + session.training.title
+            })
+          else
+            morning = [session.start_time]
+            morning_duration = session.workshops.where('position < ?', break_position).map(&:duration).sum
+            morning << session.start_time + morning_duration.minutes
+            afternoon = [session.end_time - session.workshops.where('position > ?', break_position).map(&:duration).sum.minutes, session.end_time]
+            [morning.change(day: day, month: month, year: year), afternoon.change(day: day, month: month, year: year)].each do |event|
+              events << Google::Apis::CalendarV3::Event.new({
+                start: {
+                  date_time: start_time.rfc3339.slice(0...-1),
+                  time_zone: 'Europe/Paris',
+                },
+                end: {
+                  date_time: end_time.rfc3339.slice(0...-1),
+                  time_zone: 'Europe/Paris',
+                },
+                summary: session.training.client_company.name + " - " + session.training.title
+              })
+            end
+          end
+          events.each do |event|
+            begin
+              if User.where(access_level: ['super admin', 'admin']).map{|x| x.id.to_s}.include?(ind)
+                create_calendar_id(ind, session.id, event, service, calendars_ids)
+              else
+                sevener = User.find(ind)
+                initials = sevener.initials
+                event.summary = session.training.client_company.name + " - " + session.training.title + " - " + initials
+                event.id = SecureRandom.hex(32)
+                session_trainer = SessionTrainer.where(user_id: sevener.id, session_id: session.id).first
+                session_trainer.calendar_uuid.nil? ? session_trainer.update(calendar_uuid: event.id) : session_trainer.update(calendar_uuid: session_trainer.calendar_uuid + ' - ' + event.id)
+                service.insert_event(calendars_ids['other'], event)
+              end
+            rescue
+            end
+          end
+        end
+      end
+      redirect_to training_path(training)
+      return
+    end
     redirect_to training_path(training)
   end
 
@@ -31,26 +138,18 @@ class SessionTrainersController < ApplicationController
     @session_trainer = SessionTrainer.new
     authorize @session_trainer
     @session = Session.find(params[:session_id])
-    # Select all Users whose checkbox is unchecked and destroy their SessionTrainer, if existing
-    SessionTrainer.where(session_id: @session.id).map{|x| x.user.id}.each do |ind|
-      unless SessionTrainer.where(session_id: @session.id, user_id: ind).empty?
-        to_delete = SessionTrainer.where(session_id: @session.id, user_id: ind).first
-        unless to_delete.calendar_uuid.nil?
-          @session.training.gdrive_link.nil? ? @session.training.update(gdrive_link: ind.to_s + ':' + to_delete.calendar_uuid + ',') : @session.training.update(gdrive_link: @session.training.gdrive_link + ind.to_s + ':' + to_delete.calendar_uuid + ',')
-        end
-        to_delete.destroy
+    user_ids = params[:session][:user_ids].reject{|c| c.empty?}
+    # Select all Users whose checkbox is checked and create a SessionTrainer
+    user_ids.each do |user_id|
+      unless SessionTrainer.find_by(user_id: user_id, session_id: @session.id).present?
+        SessionTrainer.create(user_id: user_id, session_id: @session.id)
       end
     end
-    # Select all Users whose checkbox is checked and create a SessionTrainer
-    array = params[:session][:user_ids].reject(&:empty?).map(&:to_i)
-    array.each do |ind|
-      if SessionTrainer.where(session_id: @session.id, user_id: ind).empty?
-        if @session.training.client_contact.client_company.client_company_type == 'Company'
-          session_trainer = SessionTrainer.create(session_id: @session.id, user_id: ind, unit_price: 80)
-        else
-          session_trainer = SessionTrainer.create(session_id: @session.id, user_id: ind, unit_price: 40)
-        end
-      end
+    # Select all Users whose checkbox is unchecked and destroy their SessionTrainer, if existing
+    to_delete = SessionTrainer.where(session_id: @session.id).where.not(user_id: user_ids)
+    if to_delete.present?
+      redirect_to redirect_path(training_id: "/#{@session.training.id}/", session_id: "|#{@session.id}|", list: "remove_trainers", user_ids: "##{to_delete.map(&:id).join(',')}#")
+      return
     end
     # UpdateAirtableJob.perform_async(@session.training, true)
     redirect_back(fallback_location: root_path)
@@ -61,29 +160,19 @@ class SessionTrainersController < ApplicationController
     authorize @session_trainer
     training = Training.find(params[:training_id])
     # Select all Users whose checkbox is checked and create a SessionTrainer
-    array = params[:training][:user_ids].drop(1).map(&:to_i)
-    array.each do |ind|
+    user_ids = params[:training][:user_ids].reject{|c| c.empty?}
+    user_ids.each do |user_id|
       training.sessions.each do |session|
-        if SessionTrainer.where(session_id: session.id, user_id: ind).empty?
-          if training.client_contact.client_company.client_company_type == 'Company'
-            session_trainer = SessionTrainer.create(session_id: session.id, user_id: ind, unit_price: 80)
-          else
-            session_trainer = SessionTrainer.create(session_id: session.id, user_id: ind, unit_price: 40)
-          end
+        unless SessionTrainer.find_by(user_id: user_id, session_id: session.id).present?
+          SessionTrainer.create(user_id: user_id, session_id: session.id)
         end
       end
     end
     # Select all Users whose checkbox is unchecked and destroy their SessionTrainer, if existing
-    (User.ids - array).each do |ind|
-      training.sessions.each do |session|
-        unless SessionTrainer.where(session_id: session.id, user_id: ind).empty?
-          to_delete = SessionTrainer.where(session_id: session.id, user_id: ind).first
-          unless to_delete.calendar_uuid.nil?
-            session.training.gdrive_link.nil? ? session.training.update(gdrive_link: ind.to_s + ':' + to_delete.calendar_uuid + ',') : session.training.update(gdrive_link: session.training.gdrive_link + ind.to_s + ':' + to_delete.calendar_uuid + ',')
-          end
-          to_delete.destroy
-        end
-      end
+    to_delete = SessionTrainer.where(session_id: training.sessions.ids).where.not(user_id: user_ids)
+    if to_delete.present?
+      redirect_to redirect_path(training_id: "/#{training.id}/", session_id: "|#{training.sessions.ids.join(',')}|", list: "remove_trainers_global", user_ids: "##{to_delete.map(&:id).join(',')}#")
+      return
     end
     # UpdateAirtableJob.perform_async(training, true)
     redirect_to training_path(training)
@@ -93,13 +182,12 @@ class SessionTrainersController < ApplicationController
     @session_trainer = SessionTrainer.new
     authorize @session_trainer
     training = Training.find(params[:training_id])
-    event_to_delete = ''
     sessions_ids = ''
     training.sessions.each do |session|
       sessions_ids += session.id.to_s + ','
     end
-    event_to_delete = training.gdrive_link[0...-1] unless !training.gdrive_link.present?
-    training.update(gdrive_link: '')
+    # event_to_delete = training.gdrive_link[0...-1] unless !training.gdrive_link.present?
+    # training.update(gdrive_link: '')
 
 
     trainers_list = []
@@ -110,7 +198,7 @@ class SessionTrainersController < ApplicationController
     # training.trainers.each{|y| training.export_numbers_sevener(y)}
     # training.export_airtable
     # training.export_numbers_activity
-    redirect_to redirect_path(list: trainers_list.join(','), session_id: "|#{training.sessions.ids.join(',')}|", to_delete: "%#{event_to_delete}%")
+    redirect_to redirect_path(training_id: "/#{training.id}/", session_id: "|#{training.sessions.ids.join(',')}|", list: trainers_list.join(','))
   end
 
   def remove_session_trainers
@@ -118,39 +206,28 @@ class SessionTrainersController < ApplicationController
     @session = Session.find(params[:session_id])
     training = @session.training
 
-    event_to_delete = ''
-    SessionTrainer.where(session_id: @session.id).each do |trainer|
-      event_to_delete+=trainer.user_id.to_s+':'+trainer.calendar_uuid+',' if trainer.calendar_uuid.present?
-    end
-    event_to_delete = event_to_delete[0...-1]
-
     UpdateAirtableJob.perform_async(@session.training, true)
     # @session.training.export_airtable
     # @session.training.export_trainer_airtable
-    if params[:destroy] == 'true'
-      @session.destroy
-    end
-    redirect_to redirect_path(session_id: "|#{@session.id}|", list: "purge_session,#{training.id}", to_delete: "%#{event_to_delete}%")
+    # if params[:destroy] == 'true'
+    #   @session.destroy
+    # end
+    redirect_to redirect_path(training_id: "/#{training.id}/", session_id: "|#{@session.id}|", list: "purge_session")
   end
 
   def remove_training_trainers
     skip_authorization
     training = Training.find(params[:training_id])
 
-    event_to_delete = ''
     sessions_ids = ''
     training.sessions.each do |session|
-      SessionTrainer.where(session_id: session.id).each do |trainer|
-        event_to_delete+=trainer.user_id.to_s+':'+trainer.calendar_uuid+',' if trainer.calendar_uuid.present?
-      end
       sessions_ids += session.id.to_s + ','
     end
-    event_to_delete = event_to_delete[0...-1]
 
     UpdateAirtableJob.perform_async(training, true)
     # training.export_airtable
     # training.export_trainer_airtable
-    redirect_to redirect_path(session_id: "|#{sessions_ids[0...-1]}|", list: 'purge_training', to_delete: "%#{event_to_delete}%")
+    redirect_to redirect_path(training_id: "/#{training.id}/", session_id: "|#{sessions_ids[0...-1]}|", list: 'purge_training')
   end
 
   def update
@@ -178,6 +255,14 @@ class SessionTrainersController < ApplicationController
     session_trainer = SessionTrainer.where(user_id: user_id, session_id: session_id).first
     session_trainer.calendar_uuid.nil? ? session_trainer.update(calendar_uuid: event.id) : session_trainer.update(calendar_uuid: session_trainer.calendar_uuid + ' - ' + event.id)
     service.insert_event(hash[user_id.to_i], event)
+  end
+
+  def delete_calendar_id(session_trainer, service)
+    if ['super admin', 'admin'].include?(session_trainer.user.access_level)
+      service.delete_event(session_trainer.user.email, session_trainer.calendar_uuid)
+    else
+      service.delete_event(calendars_ids['other'], session_trainer.calendar_uuid)
+    end
   end
 
   def session_trainer_params
--- app/jobs/update_calendar_job.rb
+++ app/jobs/update_calendar_job.rb
@@ -2,7 +2,7 @@ class UpdateCalendarJob < ApplicationJob
   include SuckerPunch::Job
 
   def perform(code, state, scope, client_options)
-   # Gets clearance from OAuth
+    # Gets clearance from OAuth
     client = Signet::OAuth2::Client.new(client_options)
     client.code = code
     client.update!(client.fetch_access_token!)
@@ -11,56 +11,69 @@ class UpdateCalendarJob < ApplicationJob
     service.authorization = client
     # Get the targeted session
     session_ids = Base64.decode64(state).split('|')[1].split(',').map{|x| x.to_i}
-    command = Base64.decode64(state).split('|').first.split(',').first
-    training_id = Base64.decode64(params[:state]).split('|').first&.split(',')&.last
-    training = Training.find(training_id)
-    training = Session.find(session_ids[0]).training unless training.present?
+    command = Base64.decode64(state).split('|').first
     # Calendars ids
     calendars_ids = {'other' => 'vum1670hi88jgei65u5uedb988@group.calendar.google.com'}
     User.where(access_level: ['super admin','admin']).each{|x| calendars_ids[x.id] = x.email}
     # Lists the users and the ids of the events to be deleted
-    SessionTrainer.where(session_id: session_ids).each do |session_trainer|
-      begin
-        if ['super admin', 'admin'].include?(session_trainer.user.access_level)
-          service.delete_event(session_trainer.user.email, session_trainer.calendar_uuid)
-        else
-          service.delete_event(calendars_ids['other'], session_trainer.calendar_uuid)
-        end
-        session_trainer.update(calendar_uuid: nil)
-      rescue
+    if command[0...-1] == 'purge_session'
+      SessionTrainer.where(session_id: session_ids).each do |session_trainer|
+        # begin
+          delete_calendar_id(session_trainer, service)
+        # rescue
+        # end
       end
-    end
-    if command == 'purge_session' || command == 'purge_training'
-      to_delete_string = Base64.decode64(state).split('|').last.split('%').last
-
-      # Deletes the events
-      to_delete_string.split(',').each do |pair|
-        key = pair.split(':')[0]
-        value = pair.split(':')[1]
-        begin
-          if User.where(access_level: ['super admin', 'admin']).map{|x| x.id.to_s}.include?(key)
-            service.delete_event(calendars_ids[key.to_i], value) if value.present?
-          else
-            service.delete_event(calendars_ids['other'], value) if value.present?
-          end
-        rescue
-        end
+      Session.find(session_ids.join).destroy
+      redirect_to training_path(training)
+      return
+    elsif command[0...-1] == 'remove_trainers' || command[0...-1] == 'remove_trainers_global'
+      trainers_to_delete = Base64.decode64(state).split('#')[1].split(',')
+      SessionTrainer.where(id: trainers_to_delete).each do |session_trainer|
+        # begin
+          delete_calendar_id(session_trainer, service)
+        # rescue
+        # end
+        session_trainer.destroy
       end
+      redirect_to training_path(training)
       return
     else
+      SessionTrainer.where(session_id: session_ids).each do |session_trainer|
+        # begin
+          delete_calendar_id(session_trainer, service)
+          session_trainer.update(calendar_uuid: nil)
+        # rescue
+        # end
+      end
       # Lists the users for whom an event will be created
       list = command.split(',')
       # Creates the event in all the targeted calendars
       list.each do |ind|
         Session.where(id: session_ids).each do |session|
-          unless SessionTrainer.where(session_id: session.id, user_id: ind.to_i).first&.calendar_uuid&.present?
-            date = session&.date
-            day, month, year = date.day, date.month, date.year
-            start_time = session.start_time.change(day: day, month: month, year: year)
-            end_time = session.end_time.change(day: day, month: month, year: year)
-            events = []
-            break_position = session.workshops.find_by(title: 'Pause Déjeuner')&.position
-            if break_position.nil?
+          date = session&.date
+          day, month, year = date.day, date.month, date.year
+          start_time = session.start_time.change(day: day, month: month, year: year)
+          end_time = session.end_time.change(day: day, month: month, year: year)
+          events = []
+          break_position = session.workshops.find_by(title: 'Pause Déjeuner')&.position
+          if break_position.nil?
+            events << Google::Apis::CalendarV3::Event.new({
+              start: {
+                date_time: start_time.rfc3339.slice(0...-1),
+                time_zone: 'Europe/Paris',
+              },
+              end: {
+                date_time: end_time.rfc3339.slice(0...-1),
+                time_zone: 'Europe/Paris',
+              },
+              summary: session.training.client_company.name + " - " + session.training.title
+            })
+          else
+            morning = [session.start_time]
+            morning_duration = session.workshops.where('position < ?', break_position).map(&:duration).sum
+            morning << session.start_time + morning_duration.minutes
+            afternoon = [session.end_time - session.workshops.where('position > ?', break_position).map(&:duration).sum.minutes, session.end_time]
+            [morning.change(day: day, month: month, year: year), afternoon.change(day: day, month: month, year: year)].each do |event|
               events << Google::Apis::CalendarV3::Event.new({
                 start: {
                   date_time: start_time.rfc3339.slice(0...-1),
@@ -72,26 +85,10 @@ class UpdateCalendarJob < ApplicationJob
                 },
                 summary: session.training.client_company.name + " - " + session.training.title
               })
-            else
-              morning = [session.start_time]
-              morning_duration = session.workshops.where('position < ?', break_position).map(&:duration).sum
-              morning << session.start_time + morning_duration.minutes
-              afternoon = [session.end_time - session.workshops.where('position > ?', break_position).map(&:duration).sum.minutes, session.end_time]
-              [morning.change(day: day, month: month, year: year), afternoon.change(day: day, month: month, year: year)].each do |event|
-                events << Google::Apis::CalendarV3::Event.new({
-                  start: {
-                    date_time: start_time.rfc3339.slice(0...-1),
-                    time_zone: 'Europe/Paris',
-                  },
-                  end: {
-                    date_time: end_time.rfc3339.slice(0...-1),
-                    time_zone: 'Europe/Paris',
-                  },
-                  summary: session.training.client_company.name + " - " + session.training.title
-                })
-              end
             end
-            events.each do |event|
+          end
+          events.each do |event|
+            # begin
               if User.where(access_level: ['super admin', 'admin']).map{|x| x.id.to_s}.include?(ind)
                 create_calendar_id(ind, session.id, event, service, calendars_ids)
               else
@@ -103,12 +100,14 @@ class UpdateCalendarJob < ApplicationJob
                 session_trainer.calendar_uuid.nil? ? session_trainer.update(calendar_uuid: event.id) : session_trainer.update(calendar_uuid: session_trainer.calendar_uuid + ' - ' + event.id)
                 service.insert_event(calendars_ids['other'], event)
               end
-            end
+            # rescue
+            # end
           end
         end
       end
       return
     end
+    redirect_to training_path(training)
   end
 
   private
@@ -118,5 +117,13 @@ class UpdateCalendarJob < ApplicationJob
     session_trainer = SessionTrainer.where(user_id: user_id, session_id: session_id).first
     session_trainer.calendar_uuid.nil? ? session_trainer.update(calendar_uuid: event.id) : session_trainer.update(calendar_uuid: session_trainer.calendar_uuid + ' - ' + event.id)
     service.insert_event(hash[user_id.to_i], event)
+  end
+
+  def delete_calendar_id(session_trainer, service)
+    if ['super admin', 'admin'].include?(session_trainer.user.access_level)
+      service.delete_event(session_trainer.user.email, session_trainer.calendar_uuid)
+    else
+      service.delete_event(calendars_ids['other'], session_trainer.calendar_uuid)
+    end
   end
 end
