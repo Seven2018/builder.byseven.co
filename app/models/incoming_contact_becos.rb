9cb36a580b90f8bb5094d681ade721f150a4b408
Brice Chapuis <bricechapuis>
2021-10-04 09:53:19 +0200

    becos contact_form test

--- app/models/incoming_contact_becos.rb
+++ app/models/incoming_contact_becos.rb
@@ -1,6 +1,6 @@
 Airrecord.api_key = Rails.application.credentials.airtable_key
 
-class IncomingContact < Airrecord::Table
+class IncomingContactBecos < Airrecord::Table
   self.base_key = 'appYT5PVZu72h7ECX'
   self.table_name = 'Contacts'
 end
