0687ba4a33d0911df619c7ed70ebe17852d07f53
Bryce <brice.chapuis@gmail.com>
2022-12-06 16:25:00 +0100

    InvoiceItem Edit Company button disabled input is empty

--- app/javascript/controllers/invoices/invoices_show_controller.js
+++ app/javascript/controllers/invoices/invoices_show_controller.js
@@ -16,4 +16,27 @@ export default class extends Controller {
 
     this.toggleEditObject()
   }
+
+  enableInvoiceEdit(event) {
+    const element = event.currentTarget
+    const form = element.closest('form')
+    const button = form.querySelector('button[type="submit"]')
+
+    if (element.value != '') {
+      button.disabled = false
+      button.classList.remove('disabled')
+    } else {
+      button.disabled = true
+      button.classList.add('disabled')
+    }
+  }
+
+  disableInvoiceEdit(event) {
+    const element = event.currentTarget
+    const form = element.closest('form')
+    const button = form.querySelector('button[type="submit"]')
+
+    button.disabled = true
+    button.classList.add('disabled')
+  }
 }
